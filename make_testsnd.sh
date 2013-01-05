16e3783ce7a02ba57e7fbb7fa03a563cb62b859f
Yiling Cao <yiling.cao@gmail.com>
2013-01-05 16:55:25 +0800

    add dB_meter.

--- make_testsnd.sh
+++ make_testsnd.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#sudo apt-get install libsndfile1-dev 
 gcc  -I/usr/local/include -O2 -c testsnd.c 
-libtool  --mode=link gcc  -g -O2 -std=gnu99 -Wall -Wextra -Wstrict-prototypes -Wmissing-prototypes -Waggregate-return -Wcast-align -Wcast-qual -Wnested-externs -Wshadow -Wpointer-arith -o testsnd ./testsnd.o -L/usr/local/lib -lsndfile   -ljack -lpthread -lrt   -lm 
+libtool  --mode=link gcc  -g -O2 -std=gnu99 -Wall -Wextra -Wstrict-prototypes -Wmissing-prototypes -Waggregate-return -Wcast-align -Wcast-qual -Wnested-externs -Wshadow -Wpointer-arith -o testsnd ./testsnd.o -L/usr/local/lib -lsndfile   -lpthread -lrt   -lm 
 
+#make db meter
+gcc dB_meter.c -lasound -o dB_meter
